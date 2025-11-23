e00b3acb713ca9e40c45923642fcf27e9ce17d18
NANOBANA <yuuya.ishikawa@gmail.com>
2025-11-23 22:33:02 +0900

    bugfix: alloc_pagesにおいてnext_free += sizeしたあとにnext_free + size > end_addrを見ているため一回分多く確認しているバグを修正。

--- mm/alloc.c
+++ mm/alloc.c
@@ -5,19 +5,19 @@
 paddr32_t alloc_pages(size_t num_pages) 
 {
     static paddr32_t next_free = (paddr32_t)&__free_ram;
-    paddr32_t addr = next_free;
     paddr32_t end_addr = (paddr32_t)&__free_ram_end;
-    next_free += num_pages * 4096;
 
-    size_t size = num_pages * 4096; // ページサイズは4KB
+    size_t size = num_pages * PAGE_SIZE;
 
     if (next_free + size > end_addr) 
     {
-        // メモリ不足
         panic("Out of memory in alloc_pages");
     }
 
-    memset((void *)addr, 0, size); // 確保したメモリをゼロ初期化
+    paddr32_t addr = next_free;
+    next_free += size;
+
+    memset((void *)addr, 0, size);
     return addr;
 }
 
